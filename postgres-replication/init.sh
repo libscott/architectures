c634e07c68b1efff97609b5210f0db2a4bb31f96
Scott Sadler <scott@alphanode.co>
2016-08-14 03:07:54 +0200

    fix indentations

--- postgres-replication/init.sh
+++ postgres-replication/init.sh
@@ -16,31 +16,31 @@ if [ $role == master ]; then
 
     echo "Create archive folder"
     $P mkdir "$pgdata/archive"
-	echo "Start server"
-	/etc/init.d/postgresql start
+    echo "Start server"
+    /etc/init.d/postgresql start
     echo "Create replicator user"
-	$P psql -c "CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'thepassword';"
+    $P psql -c "CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'thepassword';"
     echo "Master ready"
-	echo '' | nc -l 9001
+    echo '' | nc -l 9001
 fi
 
 
 if [ $role == slave ]; then
     echo "Create .pgpass"
-	$P bash -c 'echo "postgres-master:*:*:*:thepassword" > ~postgres/.pgpass && chmod 600 ~postgres/.pgpass'
+    $P bash -c 'echo "postgres-master:*:*:*:thepassword" > ~postgres/.pgpass && chmod 600 ~postgres/.pgpass'
 
-	echo "Waiting for master"
-	./wait-for-it.sh postgres-master:9001 -s -t 0
+    echo "Waiting for master"
+    ./wait-for-it.sh postgres-master:9001 -s -t 0
 
     echo "Starting base backup as replicator"
     $P rm -rf $pgdata
     $P pg_basebackup -h postgres-master -D $pgdata -U replicator -v -X stream
 
     echo "Write recovery.conf file"
-	cp /etc/postgresql/9.5/main/recovery.conf $pgdata/
+    cp /etc/postgresql/9.5/main/recovery.conf $pgdata/
 
-	echo "Start server"
-	/etc/init.d/postgresql start
+    echo "Start server"
+    /etc/init.d/postgresql start
     
     echo "Slave ready"
 fi
