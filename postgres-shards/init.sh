3c14ffa59379d187d86a8a524c24c7e69629fec5
Scott Sadler <scott@alphanode.co>
2016-08-14 02:56:40 +0200

    style init.sh

--- postgres-shards/init.sh
+++ postgres-shards/init.sh
@@ -3,7 +3,7 @@
 set -e
 
 role=$1
-pg_data=/var/lib/postgresql/9.5/main
+pgdata=/var/lib/postgresql/9.5/main
 
 P="sudo -u postgres"
 # Place configs
@@ -14,40 +14,38 @@ chown -R postgres:postgres /etc/postgresql/*
 
 if [ $role == master ]; then
 
-    echo Create archive folder
-    $P mkdir "$pg_data/archive"
-	echo Start server
+    echo "Create archive folder"
+    $P mkdir "$pgdata/archive"
+	echo "Start server"
 	/etc/init.d/postgresql start
-    echo Create replicator user
+    echo "Create replicator user"
 	$P psql -c "CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'thepassword';"
-    echo Master ready
+    echo "Master ready"
 	echo '' | nc -l 9001
 fi
 
 
 if [ $role == slave ]; then
-    echo Creating .pgpass
+    echo "Create .pgpass"
 	$P bash -c 'echo "postgres-shards-master:*:*:*:thepassword" > ~postgres/.pgpass && chmod 600 ~postgres/.pgpass'
 
-	echo Waiting for master
+	echo "Waiting for master"
 	./wait-for-it.sh postgres-shards-master:9001 -s -t 0
 
-    echo Starting base backup as replicator
-    $P rm -rf $pg_data
-    $P pg_basebackup -h postgres-shards-master -D $pg_data -U replicator -v -X stream
+    echo "Starting base backup as replicator"
+    $P rm -rf $pgdata
+    $P pg_basebackup -h postgres-shards-master -D $pgdata -U replicator -v -X stream
 
-    echo Writing recovery.conf file
-	cp /etc/postgresql/9.5/main/recovery.conf $pg_data/
+    echo "Write recovery.conf file"
+	cp /etc/postgresql/9.5/main/recovery.conf $pgdata/
 
-	echo Start server
+	echo "Start server"
 	/etc/init.d/postgresql start
     
-    echo Slave ready
+    echo "Slave ready"
 fi
 
-
-
-# Monitor
+echo "Monitoring"
 cd /var/log
 tail -f postgresql/postgresql-9.5-main.log
 
