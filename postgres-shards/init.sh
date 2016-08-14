9c84bf66fd5394deb73ff8493d5fb4f396595299
Scott Sadler <scott@alphanode.co>
2016-08-14 02:20:36 +0200

    Make it work by turning on archiving

--- postgres-shards/init.sh
+++ postgres-shards/init.sh
@@ -5,44 +5,44 @@ set -e
 role=$1
 pg_data=/var/lib/postgresql/9.5/main
 
+P="sudo -u postgres"
 # Place configs
 cd /code
 mv /etc/postgresql /etc/postgresql.bak
 cp -r postgresql-$role /etc/postgresql
 chown -R postgres:postgres /etc/postgresql/*
-sudo -u postgres rm -rf $pg_data
 
 if [ $role == master ]; then
 
-	rm -rf /var/lib/postgresql/*
-	sudo -u postgres /usr/lib/postgresql/9.5/bin/initdb $pg_data
-	find /var/lib/postgresql
-	# Start server
+    echo Create archive folder
+    $P mkdir "$pg_data/archive"
+	echo Start server
 	/etc/init.d/postgresql start
-
-	sudo -u postgres psql -c "CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'thepassword';"
-	echo 'hi' | nc -l 9001
+    echo Create replicator user
+	$P psql -c "CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'thepassword';"
+    echo Master ready
+	echo '' | nc -l 9001
 fi
 
 
 if [ $role == slave ]; then
-	sudo -u postgres bash -c 'echo "postgres-shards-master:*:*:*:thepassword" > ~postgres/.pgpass && chmod 600 ~postgres/.pgpass'
+    echo Creating .pgpass
+	$P bash -c 'echo "postgres-shards-master:*:*:*:thepassword" > ~postgres/.pgpass && chmod 600 ~postgres/.pgpass'
 
 	echo Waiting for master
 	./wait-for-it.sh postgres-shards-master:9001 -s -t 0
 
     echo Starting base backup as replicator
-    sudo -u postgres pg_basebackup -h postgres-shards-master -D $pg_data -U replicator -v -P
+    $P rm -rf $pg_data
+    $P pg_basebackup -h postgres-shards-master -D $pg_data -U replicator -v -X stream
 
     echo Writing recovery.conf file
 	cp /etc/postgresql/9.5/main/recovery.conf $pg_data/
 
-	echo Preparing data directory
-    rm $pg_data/backup_label
-	sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_resetxlog -f $pg_data
-
-	# Start server
+	echo Start server
 	/etc/init.d/postgresql start
+    
+    echo Slave ready
 fi
 
 
